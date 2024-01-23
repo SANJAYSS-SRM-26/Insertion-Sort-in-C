b6fad766065562577d02292b997df55b24a4f992
SANJAY S S <145862723+SANJAYSS-SRM-26@users.noreply.github.com>
2024-01-23 22:06:02 +0530

    Add files via upload

---
+++ InsertionSort.c
@@ -0,0 +1,44 @@
+// Insertion Sort In C
+
+#include <math.h>
+#include <stdio.h>
+ 
+//Sort Function
+
+void Sort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) 
+    {
+        key = arr[i];
+        j = i - 1;
+ 
+// Swapping for Insertion Sort
+
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+ 
+// Display Array
+
+void Display(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = 5;
+    Sort(arr, n);
+    Display(arr, n);
+    return 0;
+}
